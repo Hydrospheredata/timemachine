cddb5473c6da68842ffd9900b202770ee76402d8
dmitryisaev <dmitryisaev@cloud.upwork.com>
2019-04-05 14:39:21 +0300

    spdlog build from git

--- src/cmake/Findspdlog.cmake
+++ src/cmake/Findspdlog.cmake
@@ -19,9 +19,9 @@ find_package(PkgConfig)
 #else()
     include(ExternalProject)
     ExternalProject_Add(spdlog
-            # GIT_REPOSITORY https://github.com/gabime/spdlog.git
-            # GIT_TAG "v1.3.1"
-            SOURCE_DIR "/app/external/spdlog"
+            GIT_REPOSITORY https://github.com/gabime/spdlog.git
+            GIT_TAG "v1.3.1"
+            # SOURCE_DIR "/app/external/spdlog"
             # URL "${source_dir}/spdlib"
             TIMEOUT 5
             CMAKE_ARGS -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER} -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
